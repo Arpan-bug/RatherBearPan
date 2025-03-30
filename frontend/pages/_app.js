069004aa53ff5b5861c0a8383987723f978cae63
Arpan Saha <arpansaha121@gmail.com>
2025-03-30 12:22:37 -0500

    🚀 Add Vercel Speed Insights for performance tracking

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,19 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "@/styles/globals.css";
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+// pages/_app.js
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
+}
+
+export default MyApp;
